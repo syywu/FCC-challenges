944cb9c9b90deab997f0e1b2ecbbeebd72f34fc2
Samantha Wu <samanthayywu@gmail.com>
2022-03-26 15:55:41 +0000

    truncate str

--- challenges.js
+++ challenges.js
@@ -211,3 +211,18 @@ function confirmEnding(str, target) {
       }
       
       repeatStringNumTimes("abc", 3);
+      
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+    // We start off with a simple if statement to determine one of two outcomes…
+    function truncateString(str, num) {
+
+        // If our string length is greater than the num we want to truncate it, we return a slice of our string starting at character 0, and ending at num. We then append our '...' to the end of the string.
+        if(str.length > num){
+          return str.slice(0, num) + "...";
+        }
+        // However, if above situation is not true, it means our string length is less than our truncation num. Therefore, we can just return the string.
+        return str;
+      }
+      
+      truncateString("A-tisket a-tasket A green and yellow basket", 8);
