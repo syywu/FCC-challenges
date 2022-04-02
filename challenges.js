0c052b6bd24ee5d4cffa3356b4e557ab48f08789
Samantha Wu <samanthayywu@gmail.com>
2022-04-02 19:20:17 +0100

    diff 2 arr solution

--- challenges.js
+++ challenges.js
@@ -281,14 +281,30 @@ function confirmEnding(str, target) {
 
       /* 
       Diff Two Arrays
-      
+
       Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 
       Note: You can return the array with its elements in any order.
-      
-      
-      
+    
       */
+
+      function diffArray(arr1, arr2) {
+        const newArr = [];
+      
+        function compareArr(first, second){
+      for(let i = 0; i < first.length; i++){
+            if (second.indexOf(first[i]) == -1){
+              newArr.push(first[i]);
+            }
+        }
+      
+        }
+         compareArr(arr1, arr2);
+         compareArr(arr2, arr1);
+      return newArr;
+      }
+      
+      console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 
       /* 
       Palindrome Checker
