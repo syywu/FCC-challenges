2a33a21777cd0c5fc5565bf3ba5e0ae04267ead4
Samantha Wu <samanthayywu@gmail.com>
2022-04-02 15:32:55 +0100

    sum all num in a range

--- challenges.js
+++ challenges.js
@@ -249,6 +249,15 @@ function confirmEnding(str, target) {
       findElement([1, 2, 3, 4], num => num % 2 === 0);
 
       /* 
+      Sum All Numbers in a Range
+      
+      We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+
+      For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+    
+      */
+
+      /* 
       Palindrome Checker
       Return true if the given string is a palindrome. Otherwise, return false.
 
