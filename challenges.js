1c175556e026121d34d3ce81a9a166ef6cc161cc
Samantha Wu <samanthayywu@gmail.com>
2022-04-10 22:07:36 +0100

    franken slice

--- challenges.js
+++ challenges.js
@@ -402,3 +402,11 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+/*You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs. */
