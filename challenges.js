0f7f27f903e4c898a1261151b346cf55497871b2
Samantha Wu <samanthayywu@gmail.com>
2022-04-10 23:56:03 +0100

    franken slice solution

--- challenges.js
+++ challenges.js
@@ -429,3 +429,9 @@ function frankenSplice(arr1, arr2, n) {
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// Remove all falsy values from an array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+// Hint: Try converting each value to a Boolean.
