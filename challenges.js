6efe01f62fd557a624f327aab6afaf2f0902c43a
Samantha Wu <samanthayywu@gmail.com>
2022-04-14 23:34:07 +0100

    get index to ins solution

--- challenges.js
+++ challenges.js
@@ -459,3 +459,21 @@ function bouncer(arr) {
 For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
           
 Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1). */
+
+function getIndexToIns(arr, num) {
+
+  // First sort the array using .sort(callbackFunction) to sort it by lowest to highest, from left to right.
+  arr.sort((a, b) => a - b);
+
+  // create a variable to store index and sets index to 0
+  let index= 0; 
+
+  for(let i =0; i < arr.length;i++){
+    if (num > arr[i]){
+      index++;
+    }
+  }
+  return index;
+}
+
+getIndexToIns([40, 60], 50);
