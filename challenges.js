cb5c031edd6b10309edbdf9dd5e878acbf8e4d44
Samantha Wu <samanthayywu@gmail.com>
2022-04-25 19:17:51 +0100

    mutation

--- challenges.js
+++ challenges.js
@@ -487,3 +487,13 @@ If the return value is negative, it means that the first value is less than the 
 A neutral value, or zero, indicates that the two values are equal, so the order does not need to change.
 
 You can actually do any operation you want inside the compare function, as long as you return a value. To sort a descending order would be +. */
+
+
+
+/* Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien. */
