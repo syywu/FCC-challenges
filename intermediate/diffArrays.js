b1e0ae99607c082a22c3dd2c85c1299c7727b826
Samantha Wu <samanthayywu@gmail.com>
2022-07-17 10:39:36 +0100

    diffArray solution 2

--- intermediate/diffArrays.js
+++ intermediate/diffArrays.js
@@ -19,3 +19,9 @@ function diffArray(arr1, arr2) {
 
   return newArr;
 }
+
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter((item) => !arr1.includes(item) || !arr2.includes(item));
+}
