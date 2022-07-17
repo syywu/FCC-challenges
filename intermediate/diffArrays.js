e28e48aaab0b0f60b8d4938278d5143e0cb7b529
Samantha Wu <samanthayywu@gmail.com>
2022-07-17 10:30:27 +0100

    diffArray solution

--- intermediate/diffArrays.js
+++ intermediate/diffArrays.js
@@ -3,3 +3,19 @@ Compare two arrays and return a new array with any items only found in one of th
 
 Note: You can return the array with its elements in any order.
 */
+
+function diffArray(arr1, arr2) {
+  const newArr = [];
+
+  function isDiff(a1, a2) {
+    for (let i = 0; i < a1.length; i++) {
+      if (!a2.includes(a1[i])) {
+        newArr.push(a1[i]);
+      }
+    }
+  }
+  isDiff(arr1, arr2);
+  isDiff(arr2, arr1);
+
+  return newArr;
+}
