41213e4808a287e4c93db978e9eb2ed6753c83f9
Samantha Wu <samanthayywu@gmail.com>
2022-07-17 10:24:39 +0100

    reverseString solution

--- basic/reverseString.js
+++ basic/reverseString.js
@@ -3,3 +3,13 @@
 // You may need to turn the string into an array before you can reverse it.
 
 // Your result must be a string.
+
+function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i > 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
