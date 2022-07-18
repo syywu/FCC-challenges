0e8ca7586ff423c69743887832bf2071824159bd
Samantha Wu <samanthayywu@gmail.com>
2022-07-18 17:53:38 +0100

    repeatStringNumTimes solution

--- basic/repeatStringNumTimes.js
+++ basic/repeatStringNumTimes.js
@@ -1,3 +1,15 @@
 /*
 Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
 */
+
+function repeatStringNumTimes(str, num) {
+  let repeat = "";
+  if (num <= 0) {
+    return repeat;
+  }
+  for (let i = 0; i < num; i++) {
+    repeat += str;
+  }
+
+  return repeat;
+}
