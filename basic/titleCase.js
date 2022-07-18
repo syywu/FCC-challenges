1f9655abbaa137786392d76ffac665dbd5f1a864
Samantha Wu <samanthayywu@gmail.com>
2022-07-18 20:24:46 +0100

    titleCase solution

--- basic/titleCase.js
+++ basic/titleCase.js
@@ -3,3 +3,14 @@ Return the provided string with the first letter of each word capitalized. Make 
 
 For the purpose of this exercise, you should also capitalize connecting words like the and of.
 */
+
+function titleCase(str) {
+  let arr = str.toLowerCase().split(" ");
+  let newStr = "";
+  for (let i = 0; i < arr.length; i++) {
+    let caps = arr[i][0].toUpperCase();
+    let slice = arr[i].slice(1, arr[i].length);
+    newStr += caps + slice + " ";
+  }
+  return newStr.trim();
+}
