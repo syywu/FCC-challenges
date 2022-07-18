f3749815c690565db3b63c8ada54796b2f792fbd
Samantha Wu <samanthayywu@gmail.com>
2022-07-18 18:01:39 +0100

    truncateString solution

--- basic/truncateString.js
+++ basic/truncateString.js
@@ -1,3 +1,13 @@
 /*
 Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
 */
+
+function truncateString(str, num) {
+  let newStr;
+  if (str.length > num) {
+    newStr = str.slice(0, num);
+    newStr += "...";
+    return newStr;
+  }
+  return str;
+}
