adcd49ff2a979b188b9bdf406aef3a6c98d7dc0d
Samantha Wu <samanthayywu@gmail.com>
2022-07-18 17:38:56 +0100

    confirmTheEnding solution

--- basic/confirmTheEnding.js
+++ basic/confirmTheEnding.js
@@ -3,3 +3,10 @@ Check if a string (first argument, str) ends with the given target string (secon
 
 This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 */
+
+function confirmEnding(str, target) {
+  let split = str.length - target.length;
+  let newStr = str.slice(split, str.length);
+  console.log(newStr);
+  return newStr === target;
+}
