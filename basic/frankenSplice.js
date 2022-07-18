24531c39aeb110c12c56ab8f0e138e672d1f35c9
Samantha Wu <samanthayywu@gmail.com>
2022-07-18 21:48:01 +0100

    frankenSplice solution

--- basic/frankenSplice.js
+++ basic/frankenSplice.js
@@ -7,3 +7,9 @@ Begin inserting elements at index n of the second array.
 
 Return the resulting array. The input arrays should remain the same after the function runs.
 */
+
+function frankenSplice(arr1, arr2, n) {
+  // first arr going into second array
+  // slice arr 2
+  return arr2.slice(0, n).concat(arr1).concat(arr2.slice(n, arr2.length));
+}
