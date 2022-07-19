f98e3c54d3bff131936e174abd5c3851aea4f78f
Samantha Wu <samanthayywu@gmail.com>
2022-07-19 20:10:05 +0100

    chunkyMonkey solution 2

--- basic/chunkyMonkey.js
+++ basic/chunkyMonkey.js
@@ -9,3 +9,11 @@ function chunkArrayInGroups(arr, size) {
   }
   return newArr;
 }
+
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
