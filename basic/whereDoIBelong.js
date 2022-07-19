f6a6349278470e52ba400f01970bacec23830488
Samantha Wu <samanthayywu@gmail.com>
2022-07-19 09:36:04 +0100

    whereDoIBelong solution

--- basic/whereDoIBelong.js
+++ basic/whereDoIBelong.js
@@ -5,3 +5,15 @@ For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater
 
 Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
 */
+
+function getIndexToIns(arr, num) {
+  arr = arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) {
+      return i;
+    } else if (arr[arr.length - 1] < num) {
+      return arr.length;
+    }
+  }
+  return 0;
+}
